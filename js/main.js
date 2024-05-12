6954f1e7f5edb6d2e48f086ff7e7e28958c0b3db
MichalMiemus <158090652+MichalMiemus@users.noreply.github.com>
2024-05-12 17:16:48 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-function convertCurrency() {
-  const currency = document.getElementById("currency").value;
-  const amount = document.getElementById("amount").value;
+document.addEventListener("DOMContentLoaded", function () {
+  const currencyForm = document.getElementById("currencyForm");
+  currencyForm.addEventListener("submit", convertCurrency);
+});
 
-  document.getElementById("result").innerHTML =
-    '<p class="mt-3 font-weight-bold" style="font-size: 1.4em;">Pobieranie kursu waluty z NBP...</p>';
+function convertCurrency(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const currency = formData.get("currency");
+  const amount = formData.get("amount");
+  const resultElement = document.getElementById("result");
+
+  if (amount <= 0) {
+    resultElement.innerHTML =
+      '<div class="alert alert-danger" role="alert" style="max-width: 300px;">Podaj kwotę większą od zera.</div>';
+    return;
+  }
+  resultElement.innerHTML =
+    "<p><strong>Pobieranie kursu waluty z NBP...</strong></p>";
 
   fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${currency}/?format=json`)
     .then((response) => response.json())
     .then((data) => {
-      if (data.length <= 0) {
-        alert("Błedne dane z API");
-        return;
-      }
-      const rate = data.rates[0].mid;
-      if (amount <= 0) {
-        alert("Podaj kwotę większą od zera");
+      const rate = data?.rates?.[0]?.mid;
+      if (!rate) {
+        resultElement.innerHTML =
+          '<div class="alert alert-danger" role="alert" style="max-width: 300px;">Błędne dane z API.</div>';
         return;
       }
       const result = amount * rate;
-      document.getElementById(
-        "result"
-      ).innerHTML = `<p class="mt-3 font-weight-bold" style="font-size: 1.4em;"><span class="text-secondary">${amount} ${currency}</span> = ${result.toFixed(
+      resultElement.innerHTML = `<p class="mt-3 font-weight-bold" style="font-size: 1.4em;">${amount} ${currency} = ${result.toFixed(
         2
       )} PLN</p>`;
     })
     .catch((error) => {
       console.error("Wystąpił błąd podczas pobierania kursu waluty:", error);
-      document.getElementById("result").innerText =
-        "Wystąpił błąd. Spróbuj ponownie później.";
+      resultElement.innerText = "Wystąpił błąd. Spróbuj ponownie później.";
     });
 }
